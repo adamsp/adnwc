ab1c097f6cc6fa9e04019b2b8b790f3e2c49c0fe
Adam Speakman <adam.speakman@gmail.com>
2013-04-24 06:13:43 +1200

    Now retrieving all posts since last check, not just latest 200

--- data_retrieval.py
+++ data_retrieval.py
@@ -18,29 +18,41 @@ class DataRetriever:
         self.POST_COUNT = 200
         
     def get_latest_url(self, post_id):
-        url = ["https://alpha-api.app.net/stream/0/posts/stream/global?since_id="]
-        url.append(`post_id`)
-        url.append("&count=")
+        url = ["https://alpha-api.app.net/stream/0/posts/stream/global"]
+        
+        # Note we do negative count - returns up to 200 from previous max towards newest.
+        # If we do positive count, it counts down from latest post towards the requested
+        # 'since' post. Easier to count from the 'since' post towards newest.
+        # If first time run though, we just do latest 200.
+        if post_id > 0:
+            url.append("?since_id=")
+            url.append(`post_id`)
+            url.append("&count=-")
+        else:
+            url.append("?count=")
         url.append(`self.POST_COUNT`)
         return ''.join(url)
         
     def retrieve_latest_data(self):
-        url = self.get_latest_url(self.prev_max_post_id)
-        response = urlfetch.fetch(url)
-        if not response.status_code == 200:
-            logging.error('Request failed with status code ' + str(response.status_code)
-                          + ' and content: ' + str(response.content))
-            return
-        result = json.loads(response.content)
-        if result.has_key("data"):
-            posts = result["data"]
-            if len(posts) == 0:
+        more_posts = True
+        while more_posts:
+            url = self.get_latest_url(self.prev_max_post_id)
+            response = urlfetch.fetch(url)
+            if not response.status_code == 200:
+                logging.error('Request failed with status code ' + str(response.status_code)
+                              + ' and content: ' + str(response.content))
                 return
-            new_max_post_id = int(result["meta"]["max_id"])
-            self.update_prev_max_post_id(new_max_post_id)
-            for processor in self.processors:
-                processor.process_posts(posts)
-            
-    def update_prev_max_post_id(self, post_id):
-        if post_id > self.prev_max_post_id:
-            self.prev_max_post_id = post_id
+            result = json.loads(response.content)
+            if result.has_key("data"):
+                posts = result["data"]
+                if len(posts) == 0:
+                    return
+                for processor in self.processors:
+                    processor.process_posts(posts)
+            # We want to short-circuit this if this is the first time
+            # we've run - otherwise we'll end up scanning *all* ADN posts.
+            if self.prev_max_post_id > 0:
+                more_posts = result["meta"]["more"]
+            else:
+                more_posts = False
+            self.prev_max_post_id = int(result["meta"]["max_id"])
